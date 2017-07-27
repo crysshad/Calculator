8c1e3e8e3e7be53faa71d359333b36fbeb5aea48
Jing <Jing@10.0.0.142>
2017-07-26 21:07:42 -0400

    7_26_2017_commit2

---
+++ Cal/src/controller/Calculator.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class Calculator {
+
+	public double Addition(double number1, double number2) {
+
+		return number1 + number2;
+
+	}
+	
+	public double subtraction(double number1, double number2) {
+
+		return number1 - number2;
+
+	}
+	
+	public double multiplication(double number1, double number2) {
+
+		return number1 * number2;
+
+	}
+	
+	public double Division(double number1, double number2) {
+
+		return number1 / number2;
+
+	}
+}
