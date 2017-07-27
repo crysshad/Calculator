d8fb7f551afb49854be05d42e02ee4ad0af768e0
Jing <Jing@10.0.0.142>
2017-07-26 20:28:14 -0400

    7_26_2017commit1

---
+++ Cal/src/controller/Sum.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class Sum {
+
+	public int sum(int a, int b) {
+
+		return a + b;
+
+	}
+}
---
+++ Cal/src/controller/SumTest.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class SumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Sum sum = new Sum();
+		System.out.println("2+3=" + sum.sum(2, 3));
+		
+	}
+
+}
